b9705cfaaf6a9bca6a6eda06616255a107808b44
tjohnnyj <trace.johnson@gmail.com>
2013-09-23 21:14:52 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gastore::Application.config.secret_key_base = 'd85809c6b6bf903172c1b18dd1d1d94ee2fcbbba3842f731d0e6c2899bf8fd5b82d3f6f02ea83aa0e7caba4a9144ab191bd539b63b96f9629f22e9a2ce16de70'
